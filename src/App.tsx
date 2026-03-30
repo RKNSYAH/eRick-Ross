b8a82d6f8f12e779498b2f0cd6e3c90c9c5afb01
RKNSYAH <manusiakocak430@gmail.com>
2026-03-30 20:56:05 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex flex-row items-center justify-center '>
+      <div id="app" className="m-8 w-72 h-72 min-h-screen bg-white">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
