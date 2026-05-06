78cdfa7dfa2fb427fa0db244a346d1ccc1cb67b6
RKNSYAH <manusiakocak430@gmail.com>
2026-05-06 17:54:59 +0700

    missing file

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
-import { index, route } from "@react-router/dev/routes";
+import {type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("./App.tsx"),
   route("/chat", "./Chat/Chat.tsx"),
-];
+] satisfies RouteConfig;
