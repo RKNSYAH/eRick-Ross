c465bf33d21ac033e1b9574a145669f247834171
RKNSYAH <manusiakocak430@gmail.com>
2026-05-06 17:58:03 +0700

    route fix

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
-import {type RouteConfig, index, route } from "@react-router/dev/routes";
+import { index, route } from "@react-router/dev/routes";
 
 export default [
   index("./App.tsx"),
   route("/chat", "./Chat/Chat.tsx"),
-] satisfies RouteConfig;
+];
