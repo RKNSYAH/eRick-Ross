bb56ad72e9fa93a532b5d58b845a7c3347640090
RKNSYAH <manusiakocak430@gmail.com>
2026-05-06 18:23:46 +0700

    enable ssr

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,5 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   appDirectory: "src",
-  ssr: false, // Set to true if you want to enable Server-Side Rendering
+  ssr: true, // Set to true if you want to enable Server-Side Rendering
 } satisfies Config;
